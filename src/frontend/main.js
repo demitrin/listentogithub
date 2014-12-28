1a3be35d3045c0842b74e50e351a799f66c8fcb8
zachwe <zbwener@gmail.com>
2014-12-28 11:59:55 -0800

    basic d3 scaffolding

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,7 +1,43 @@
 var socket = require("socket.io-client")("http://localhost:3000");
-socket.on("github payload", function(data) {
-   var p = document.createElement("p");
-   p.innerText = JSON.stringify(data);
-   document.body.appendChild(p);
+var d3 = require('d3');
+var $ = require('jquery-browserify');
+var theSvg;
+
+
+var processNewData = function(newData) {
+    var bubbles = theSvg.data(newData);
+    bubbles
+        .enter().append("circle")
+            .attr("cx", Math.random() * 500)
+            .attr("cy", Math.random() * 500)
+            .attr("r", 20);
+    bubbles
+        .exit().transition()
+            .duration(500)
+            .style("opacity", 1e-6)
+            .remove();
+};
+
+var initializeSocket = function() {
+    socket.on("github payload", function(data) {
+    /*
+    var p = document.createElement("p");
+    p.innerText = JSON.stringify(data);
+    document.body.appendChild(p);
+    */
+        processNewData(data);
+    });
+
+};
+
+var initialize = function() {
+    theSvg = d3.select("#the-svg");
+    initializeSocket();
+    
+};
+
+$(document).ready(function() {
+    console.log("ready to go");
+    initialize();
+
 });
-console.log('asdfkjs');
