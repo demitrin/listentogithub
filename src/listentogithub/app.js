c1822eff74bd43673e256c6ca43614f4f2b6e1b3
Demitri Nava <navademitri@gmail.com>
2015-01-03 20:25:56 -0800

    File formatting

--- src/listentogithub/app.js
+++ src/listentogithub/app.js
@@ -72,7 +72,7 @@ app.use(function(err, req, res, next) {
 app.set('port', process.env.PORT || 3000);
 
 server.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
+    debug('Express server listening on port ' + server.address().port);
 });
 
 
